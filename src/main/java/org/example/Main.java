4c94e6f7eaaad28d44d80a1a19bcf4e42a12f17b
jotria <joana.tria@gcash.com>
2025-07-03 17:22:07 +0800

    Final Submission, Java Unit OOP1 Project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,17 +3,42 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static BankAccount findBankAccountByNumber(String accountNumber, ArrayList<BankAccount> bankAccounts) {
+        if (accountNumber == null || accountNumber.isEmpty()) {
+            return null;
+        }
+
+        boolean accountFound = false;
+
+        for (BankAccount bankAccount : bankAccounts) {
+            if (bankAccount.getAccountNumber() != null && bankAccount.getAccountNumber().equalsIgnoreCase(accountNumber)) {
+                accountFound = true;
+                return bankAccount;
+            }
+        }
+
+        if(!accountFound){
+            System.out.println("Account not found!");
+        }
+
+        return null;
+    }
+
     public static void main(String[] args) {
         ArrayList<BankAccount> bankAccounts = new ArrayList<>();
+
         int choice;
         String returnChoice = "";
+        String accountNumber = "";
+        String bankAccountHolderName = "";
+        String initialDeposit = "";
+        float addAmount = 0;
+        float deductAmount = 0;
         Scanner scanner = new Scanner(System.in);
 
         do{
-            System.out.println("=== Bank Menu ===");
+            System.out.println("\n === Bank Menu ===");
             System.out.println("1. Create Account");
             System.out.println("2. View All Accounts");
             System.out.println("3. Check Balance");
@@ -28,29 +53,78 @@ public class Main {
             switch(choice){
                 case 1:
                     System.out.print("Enter Account Number: ");
-                    int accountNumber = scanner.nextInt();
-                    scanner.nextLine();
+                    accountNumber = scanner.nextLine();
 
                     System.out.print("Enter Holder Name: ");
-                    String bankAccountHolderName = scanner.nextLine();
+                    bankAccountHolderName = scanner.nextLine();
 
                     System.out.print("Initial deposit? (yes/no): ");
-                    String initialDeposit = scanner.nextLine();
+                    initialDeposit = scanner.nextLine();
 
-                    System.out.print("Enter initial deposit amount: ");
-                    float initialDepositAmount = scanner.nextFloat();
-                    scanner.nextLine();
+                    if(initialDeposit.equals("yes")){
+                        System.out.print("Enter initial deposit amount: ");
+                        float initialDepositAmount = scanner.nextFloat();
+                        scanner.nextLine();
+
+                        bankAccounts.add(new BankAccount(accountNumber, bankAccountHolderName, initialDepositAmount));
+
+                    } else{
+                        bankAccounts.add(new BankAccount(accountNumber, bankAccountHolderName));
+                        break;
+                    }
 
                     System.out.println("Account created successfully!");
                     break;
 
                 case 2:
+                    for(BankAccount bankAccount : bankAccounts){
+                        bankAccount.displayInformation();
+                    }
                     break;
-                default:
+                case 3:
+                    System.out.print("Enter Account Number: ");
+                    accountNumber = scanner.nextLine();
+
+                    BankAccount accountBalance = findBankAccountByNumber(accountNumber, bankAccounts);
+                    try{
+                        System.out.println("Current balance of " + accountBalance.getBankAccountHolderName() + " is " + accountBalance.getAvailableBalance());
+                    } catch (NullPointerException e){
+                        System.out.println("Account not found!");
+                    }
+
+                    break;
+                case 4:
+                    System.out.print("Enter Account Number: ");
+                    accountNumber = scanner.nextLine();
+
+                    BankAccount account = findBankAccountByNumber(accountNumber, bankAccounts);
+
+                    System.out.print("How much would you like to deposit?: ");
+                    addAmount = scanner.nextFloat();
+                    scanner.nextLine();
+
+                    account.deposit(addAmount);
+                    break;
+                case 5:
+                    System.out.print("Enter Account Number: ");
+                    accountNumber = scanner.nextLine();
+
+                    BankAccount accountToWithdraw = findBankAccountByNumber(accountNumber, bankAccounts);
+
+                    System.out.print("How much would you like to withdraw?: ");
+                    deductAmount = scanner.nextFloat();
+                    scanner.nextLine();
+
+                    accountToWithdraw.withdraw(deductAmount);
+                    break;
+                case 6:
+                    System.out.println("-----Thank you for banking with us!-----");
+                    returnChoice.equalsIgnoreCase("no");
                     break;
             }
-            System.out.print("Would you like to return to the menu? (yes/no): ");
+            System.out.print("\nWould you like to return to the menu? (yes/no): ");
             returnChoice = scanner.nextLine();
+
         } while(returnChoice.equals("yes"));
     }
 }
